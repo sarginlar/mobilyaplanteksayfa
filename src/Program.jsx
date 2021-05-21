343eeed46032abd7d1c988465c571f6828eee844
RifatSargin <sarginlar@gmail.com>
2021-05-21 14:18:19 +0300

    first

--- src/Program.jsx
+++ src/Program.jsx
@@ -2543,6 +2543,7 @@ const Program = () => {
     }
     const Baza1 = () => {
       const a = mobilya.baza
+      console.log('test')
       return (
         <mesh visible userData={{ test: 'hello' }} rotation={[0, 0, 0]} position={[a.x0 / u, a.y0 / u, a.z0 / u]} castShadow>
           <boxBufferGeometry attach='geometry' args={[a.x / u, a.y / u, a.z / u]} />
